b658f545f58fbd4161f3e9a0cea1539617876f1c
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-12 21:47:30 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,8 +33,6 @@ class PetsController < ApplicationController
     @pet.update(params["pet"])
     if !params[:owner][:name].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
-    else
-      @pet.owner = Owner.find_by_id(params["pet"]["owner_id"])
     end
     @pet.save
 
